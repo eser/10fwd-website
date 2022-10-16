955a0cf41fc3795988c6c9f7b6866e057a1501bb
Eser Ozvataf <eser@ozvataf.com>
2022-10-16 14:03:13 +0300

    layout arrangements.

--- src/shared/layout/index.tsx
+++ src/shared/layout/index.tsx
@@ -1,8 +1,10 @@
 import { type ReactNode } from "react";
 import Head from "next/head";
-import { Footer } from "./footer";
+import Image from "next/future/image";
 import { type CustomAppProps } from "@webclient/pages/_app.types";
+import { Footer } from "./footer";
 import styles from "./index.module.css";
+import logoImage from "./logo.svg";
 
 interface LayoutProps {
   appProps: CustomAppProps;
@@ -18,7 +20,70 @@ const Layout = (props: LayoutProps) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <div className={styles.app}>
-        <main>{props.children}</main>
+        <main>
+          <section className={styles["hero-section"]}>
+            <div className={styles.inner}>
+              <div className={styles.content}>
+                <Image
+                  src={logoImage}
+                  alt="10forward logo"
+                  width="498"
+                  height="328"
+                  priority={true}
+                />
+                <p>
+                  2015&apos;ten bu yana topluluklar oluşturup, geliştirici
+                  ekosistemine katkıda bulunmak, topluluğu motive etmek,
+                  farkındalık aşılamak ve engelleri ortadan kaldırmak için
+                  çalışıyoruz.
+                </p>
+              </div>
+            </div>
+          </section>
+
+          <section className={styles["content-section"]}>
+            <div className={styles.inner}>
+              <div className={styles["tab-bar"]}>
+                <ul>
+                  <li className={styles["active"]}>
+                    <a href="#" aria-current="page">
+                      <svg
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z">
+                        </path>
+                      </svg>Girişimler
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <svg
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z"></path>
+                        <path
+                          fillRule="evenodd"
+                          d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
+                          clipRule="evenodd"
+                        >
+                        </path>
+                      </svg>Topluluk Sözleşmesi
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              {props.children}
+            </div>
+          </section>
+        </main>
+
         <Footer />
       </div>
     </>
