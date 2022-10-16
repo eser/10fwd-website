44d8c5dff1556d02a6daca50733d035b3349acb7
Eser Ozvataf <eser@ozvataf.com>
2022-10-16 03:48:59 +0300

    added twitter handle.

--- src/shared/defaults.ts
+++ src/shared/defaults.ts
@@ -2,6 +2,7 @@ const defaults = {
   siteName: "10forward",
   locale: "tr_TR",
   webSiteUrl: "https://10forward.io/",
+  twitterHandle: "@10forward_io",
 };
 
 export { defaults, defaults as default };
